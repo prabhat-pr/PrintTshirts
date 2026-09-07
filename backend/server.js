7449107a2dd69a7a49c273bfbf30717eaed335d4
Prabhat Ranjan <prabhat.ranjan@zohomail.in>
2026-09-07 11:33:33 +0530

    changes cjs to esm

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("PrintTshirts backend is running!");
